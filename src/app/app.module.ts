5f75d9ffc57a20c6bbedd8b2bb71d12f5865486f
Stuti Bhageria <stuts88@gmail.com>
2018-07-28 16:27:03 +0530

    Setup Angular & Created 2 new components - warning and success alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessalertComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/warningalert/warningalert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+@Component({
+  selector:'app-warningalert',
+  templateUrl:'./warningalert.component.html',
+  styleUrls:['./warningalert.component.css'],
+})
+export class WarningAlertComponent{
+
+}
