296fd1e087589b901058cd28b8de1f186067143a
Stuti Bhageria <stuts88@gmail.com>
2018-07-30 22:59:59 +0530

    Data binding assignment completed

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-server',
-  templateUrl: './server.component.html'
+  selector: "app-server",
+  templateUrl: "./server.component.html"
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = "offline";
+  allowNewServer = false;
+  newServerStatus = "No new server created";
+  serverName = "Start Server";
+  username = "";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.newServerStatus = "New server with name - "  + this.serverName + " successfully";
+  }
 
 }
